9799fd9ed1e19ca245a82875dc52d0798804fc26
Raffael Thauã <thaua23sl@gmail.com>
2026-05-28 23:59:37 -0300

    add comment

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+
+        // Testando as classes
         Loja steam = new Loja("Steam");
 
         Usuario thaua23sl = new Usuario("Thaua23sl");
